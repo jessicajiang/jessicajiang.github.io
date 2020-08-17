8d780efe18abb184ead08f9ec0fabb96fdc2b0ac
Jessica Jiang <jessicajiang5@gmail.com>
2020-08-17 11:19:50 -0700

    confused and upset

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var $ = function (id)
+{
+	return document.getElementById(id);
+}
+
 function swapText(divName) {
 	var workDes = document.getElementById("work-description")
 	workDes.style.backgroundColor = "#ffada0"
@@ -6,10 +11,11 @@ function swapText(divName) {
 	if(divName == 'mozilla') {
 		workDes.innerHTML = "Mozilla, Summer of 2017"
 		workDes.style.backgroundColor = "#e3eaf7"
+
 	} else if (divName == 'twitter') {
-		workDes.innerHTML = "Twitter, Summer of 2018"
+		workDes.innerHTML = "<div id = \"close-description\" onclick = \"close()\"> <img src=\"icons/close-button.png\" alt=\"close button\"> </div>Twitter, Summer of 2018"
 	} else if (divName == 'vsco') {
-		workDes.innerHTML = "VSCO, since Summer of 2019"
+		workDes.innerHTML = "VSCO, since Summer of 2019. <br> vsco.co/jessssji"
 		workDes.style.backgroundColor = "#e3eaf7"
 	} else {
 		// alert("Close button pressed")
@@ -18,6 +24,9 @@ function swapText(divName) {
 		// workDes.style.opacity = "0"
 		close()
 	}
+	var closeButton = document.createElement("closeDescription");
+	var closeButtonDiv = $("work-description");
+	closeButtonDiv.appendChild(closeButton)
 	// var closeButton = document.getElementById("closeDescription").src
 	// // var closeButton = document.getElementbyId("closeButton").src
 	// closeButton.style.opacity = "1"
